ac4e1d7d9410c7bd46958b91789ebd1257918729
yuanshouyan1823 <yuanshouyan1823@sohu.com>
2018-05-08 21:57:57 +0800

    Add files via upload
    bad network

---
+++ DBAccessor/DAL/DBDataSource.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace DBAccessor
+{
+
+	[Serializable]
+	public class DBInstance
+	{
+		public DatabaseType DatabaseType;
+		public string ConnectionString;
+		public string DBSoureName;
+	}
+
+	public enum DatabaseType
+	{
+		ORACLE = 0,
+		SQLSERVER = 1,
+		SQLITE = 2
+	}
+
+	public static class DataSoureNames
+	{
+		public static readonly string OA = "OA";
+		public static readonly string HR = "HR";
+		public static readonly string X3 = "X3";
+		public static readonly string SAP = "SAP";
+		public static readonly string PLM = "PLM";
+		public static readonly string MIS = "MIS";
+		public static readonly string MES = "MES";
+		public static readonly string LocalDB = "LocalDB";
+	}
+
+	public static class DataSoure
+	{
+		public static DBInstance OA => DBInstancelist?.Find(a => a.DBSoureName == DataSoureNames.OA);
+		public static DBInstance HR => DBInstancelist?.Find(a => a.DBSoureName == DataSoureNames.HR);
+		public static DBInstance SAP => DBInstancelist?.Find(a => a.DBSoureName == DataSoureNames.SAP);
+		public static DBInstance PLM => DBInstancelist?.Find(a => a.DBSoureName == DataSoureNames.PLM);
+		public static DBInstance MIS => DBInstancelist?.Find(a => a.DBSoureName == DataSoureNames.MIS);
+		public static DBInstance MES => DBInstancelist?.Find(a => a.DBSoureName == DataSoureNames.MES);
+		public static DBInstance LocalDB => DBInstancelist?.Find(a => a.DBSoureName == DataSoureNames.LocalDB);
+
+		static string GetConnectionString(String providerName)
+		{
+			var dbInstance = DBInstancelist?.Find(a => a.DBSoureName == providerName);
+			return dbInstance == null ? String.Empty : dbInstance.ConnectionString;
+		}
+
+		static List<DBInstance> DBInstancelist;
+		static DataSoure()
+		{
+			XmlSerializer xs = new XmlSerializer(typeof(List<DBInstance>));
+			using (Stream stream = new FileStream("DbConfig.xml", FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+			{
+				DBInstancelist = xs.Deserialize(stream) as List<DBInstance>;
+			}
+		}
+	}
+}
---
+++ DBAccessor/DAL/DataOperator.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+using System.Data.OracleClient;
+using System.Data.SQLite;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DBAccessor
+{
+	public class DataOperator : IDisposable
+	{
+		#region 构造和析构
+		public DataOperator(DBInstance dbInstance)
+		{
+			this.dbInstance = dbInstance;
+			DbConnection.Open();
+		}
+
+		private bool disposed;
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!disposed)
+			{
+				if (disposing)
+				{
+					if (DbConnection.State != ConnectionState.Closed)
+						DbConnection.Close();
+				}
+				disposed = true;
+			}
+		}
+
+		~DataOperator()
+		{
+			Dispose(false);
+		}
+		#endregion
+
+		private IDbConnection dbConnection;
+
+		private DBInstance dbInstance;
+
+		public String DataSoureName => dbInstance.DBSoureName;
+
+		public String ConnectionString => dbInstance.ConnectionString;
+
+		public IDbConnection DbConnection
+		{
+			get
+			{
+				if (dbConnection == null)
+				{
+					switch (dbInstance.DatabaseType)
+					{
+						case DatabaseType.ORACLE:
+							dbConnection = new OracleConnection(dbInstance.ConnectionString);
+							break;
+						case DatabaseType.SQLSERVER:
+							dbConnection = new SqlConnection(dbInstance.ConnectionString);
+							break;
+						case DatabaseType.SQLITE:
+							dbConnection = new SQLiteConnection(dbInstance.ConnectionString);
+							break;
+						default:
+							throw new Exception("Unsupported DB Type!");
+					}
+				}
+				return dbConnection;
+			}
+		}
+
+		private void PrepareCommand(IDbCommand command, CommandType commandType, string sql, IDbDataParameter[] parms)
+		{
+			if (command == null)
+			{
+				throw new ArgumentNullException("IDbCommand", "空参数异常!");
+			}
+			command.CommandText = sql;
+			command.CommandType = commandType;
+			if (parms != null && parms.Length > 0)
+			{
+				foreach (var parameter in parms)
+				{
+					if ((parameter.Direction == ParameterDirection.InputOutput || parameter.Direction == ParameterDirection.Input) && (parameter.Value == null))
+					{
+						parameter.Value = DBNull.Value;
+					}
+					command.Parameters.Add(parameter);
+				}
+			}
+		}
+
+		public DataTable ExecuteQuery(string sql, CommandType commandType = CommandType.Text, params IDbDataParameter[] parameter)
+		{
+			using (IDbCommand cmd = DbConnection.CreateCommand())
+			{
+				PrepareCommand(cmd, commandType, sql, parameter);
+				DataTable tab = new DataTable();
+				using (IDataReader reader = cmd.ExecuteReader())
+				{
+					tab.Load(reader);
+					return tab;
+				}
+			}
+		}
+
+		public int ExecuteNonQuery(string sql, CommandType commandType = CommandType.Text, params IDbDataParameter[] parameter)
+		{
+			using (IDbCommand cmd = DbConnection.CreateCommand())
+			{
+				PrepareCommand(cmd, commandType, sql, parameter);
+				return cmd.ExecuteNonQuery();
+			}
+		}
+
+		public object ExecuteScalar(string sql, CommandType commandType = CommandType.Text, params IDbDataParameter[] parameter)
+		{
+			using (IDbCommand cmd = DbConnection.CreateCommand())
+			{
+				PrepareCommand(cmd, commandType, sql, parameter);
+				return cmd.ExecuteScalar();
+			}
+		}
+
+		public IDataReader ExecuteReader(string sql, CommandType commandType = CommandType.Text, params IDbDataParameter[] parameter)
+		{
+			using (IDbCommand cmd = DbConnection.CreateCommand())
+			{
+				PrepareCommand(cmd, commandType, sql, parameter);
+				return cmd.ExecuteReader();//CommandBehavior.CloseConnection
+			}
+		}
+
+		public bool ExecuteTransaction(Hashtable sqlList)
+		{
+			using (IDbTransaction trans = DbConnection.BeginTransaction())
+			{
+				try
+				{
+					using (IDbCommand cmd = DbConnection.CreateCommand())
+					{
+						foreach (DictionaryEntry item in sqlList)
+						{
+							cmd.Parameters.Clear();
+							cmd.CommandText = item.Key.ToString();
+							if (item.Value != null)
+							{
+								var parameter = item.Value as IDbDataParameter[];
+								for (int i = 0; i < parameter.Length && parameter != null; i++)
+								{
+									cmd.Parameters.Add(parameter[i]);
+								}
+							}
+
+							cmd.Transaction = trans;
+							cmd.Connection = DbConnection;
+							cmd.ExecuteNonQuery();
+						}
+						trans.Commit();
+						return true;
+					}
+				}
+				catch (Exception ex)
+				{
+					trans.Rollback();
+					throw ex;
+				}
+			}
+		}
+
+		public bool ExecuteTransaction(IEnumerable<Tuple<string, CommandType, IDbDataParameter[]>> sqlList)
+		{
+			using (IDbTransaction trans = DbConnection.BeginTransaction())
+			{
+				try
+				{
+					using (IDbCommand cmd = DbConnection.CreateCommand())
+					{
+						foreach (var item in sqlList)
+						{
+							cmd.Parameters.Clear();
+							cmd.CommandText = item.Item1.ToString();
+							cmd.CommandType = item.Item2;
+							if (item.Item3 != null)
+							{
+								var parameter = item.Item3;
+								for (int i = 0; i < parameter.Length && parameter != null; i++)
+								{
+									cmd.Parameters.Add(parameter[i]);
+								}
+							}
+
+							cmd.Transaction = trans;
+							cmd.Connection = DbConnection;
+							cmd.ExecuteNonQuery();
+						}
+						trans.Commit();
+						return true;
+					}
+				}
+				catch (Exception ex)
+				{
+					trans.Rollback();
+					throw ex;
+				}
+			}
+		}
+
+	}
+}
